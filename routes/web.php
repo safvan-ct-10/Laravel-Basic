61a1e6880333b2503d383436fd800f8c7110db52
safvan ct <isafvanct@gmail.com>
2022-05-22 13:30:02 +0530

    Login function created

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/', 'App\Http\Controllers\LoginController@index')->name('admin.login');
+Route::post('/login', 'App\Http\Controllers\LoginController@login')->name('login.post');
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => 'admin_logged', 'namespace' => 'App\Http\Controllers'], function(){
+    Route::get('/dashboard', 'LoginController@home')->name('admin.home');
+    Route::post('/logout', 'LoginController@logout')->name('admin.logout');
 });
